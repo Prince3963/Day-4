edce79317801fa5379b2876b1a942eeb3a7bfd22
Prince3963 <princepatel3963@gmail.com>
2026-01-29 15:17:09 +0530

    Done

---
+++ Dalegates/Dalegate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_40.Dalegates
+{
+    public delegate void dalegateFunctions(string message);
+    public class Dalegate
+    {
+        public static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
-﻿using _31_40.Interface;
+﻿using System.Drawing;
+using _31_40.Dalegates;
+using _31_40.Interface;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //MethodImplementation implementation = new MethodImplementation();
+        //implementation.Method1();
+        //implementation.Method2();
+        //implementation.Method3();
+        //implementation.Method4();
 
-MethodImplementation implementation = new MethodImplementation();
-implementation.Method1();
-implementation.Method2();
-implementation.Method3();
-implementation.Method4();
+
+        //dalegateFunctions d = new dalegateFunctions(Dalegate.Hello);
+        //d("Hello from Delegate");
+
+
+        Point p1 = new Point { X = 10, Y = 20 };
+        Point p2 = p1;   // COPY
+
+        //p1.X = 50;
+        p2.X = 99;
+
+        Console.WriteLine(p1.X); // 👉 10 (unchanged)
+        Console.WriteLine(p1.Y); // 👉 10 (unchanged)
+        Console.WriteLine(p2.X); // 👉 10 (unchanged)
+        Console.WriteLine(p2.Y); // 👉 10 (unchanged)
+    }
+}
